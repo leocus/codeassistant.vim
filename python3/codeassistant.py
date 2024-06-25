bc4f7926e9ec76d67030cb350533ad6aebeb1879
Leonardo Lucio Custode <leonardo.custode@gmail.com>
2024-06-25 11:07:47 +0200

    Implement RAG

--- python3/codeassistant.py
+++ python3/codeassistant.py
@@ -1,18 +1,32 @@
+import os
 import vim
 import json
+import glob
+import pickle
 import requests
+
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import TextLoader
+from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+VECTORSTOREPATH = ".codeassistant_vectorstore/"
+ALLOWED_EXTENSIONS = [".py", ".json", ".js", ".c", ".cpp", ".java"]
 
 
 def get_config():
     return {
         "model_name": "deepseek-coder:6.7b-instruct",
         "url": "http://localhost:11434/api/chat",
-        "token": "no-token-needed"
+        "token": "no-token-needed",
+        "rag": False,  # Disabled by default to keep compatibility
+        "chunk_size": 500,  # Used for RAG
+        "rag_model": "deepseek-coder:1.3b",
     }
 
 
 class AutoComplete:
-
     def __init__(self):
         self.config = get_config()
         self.headers = {
@@ -29,6 +43,56 @@ class AutoComplete:
             ],
             "stream": False,
         }
+        
+        if self.config["rag"]:
+            # Check if the vector store exists
+            if not os.path.exists(VECTORSTOREPATH):
+                self.refresh_vectorstore()
+            else:
+                # Load existing vectorstore
+                self.vectorstore = Chroma(persist_directory=VECTORSTOREPATH, embedding_function=OllamaEmbeddings(model=self.config["rag_model"]))
+
+    def refresh_vectorstore(self):
+        splits = []
+        files = glob.glob('*', recursive=True)
+        files.extend(glob.glob('*/*', recursive=True))
+
+        for file in files:
+            # Check if the file is in the blacklist
+            to_be_used = False
+            i = 0
+            while i < len(ALLOWED_EXTENSIONS) and not to_be_used:
+                if file.endswith(ALLOWED_EXTENSIONS[i]):
+                    to_be_used = True
+                i += 1
+
+            if to_be_used:
+                # Load the file's content
+                print(f"Adding {file} to the vector store")
+                try:
+                    loader = TextLoader(file)
+                    data = loader.load()
+                    
+                    # Split the file content
+                    text_splitter = RecursiveCharacterTextSplitter(chunk_size=self.config['chunk_size'], chunk_overlap=0)
+                    splitted_doc = text_splitter.split_documents(data)
+
+                    for doc in splitted_doc:
+                        doc.page_content = f"File: {file}\n" + doc.page_content
+
+                    # Add the chunks to the list of chunks
+                    splits.extend(splitted_doc)
+
+                except Exception as e:
+                    print(e)
+
+        # Check n. of documents
+        if len(splits) > 0:
+            # Build vector store
+            self.vectorstore = Chroma.from_documents(documents=splits, embedding=OllamaEmbeddings(model=self.config["rag_model"]), persist_directory=VECTORSTOREPATH)
+            # pickle.dump(self.vectorstore, open(VECTORSTOREPATH, "wb"))
+        else:
+            self.vectorstore = None
 
     def get_selection(self, buffer_lines, start_line, end_line):
         prompt = "```\n"
@@ -38,8 +102,15 @@ class AutoComplete:
         prompt += "```"
         return prompt
 
-    def query_model(self, prompt):
+    def query_model(self, prompt, use_rag=False):
         payload = self.payload.copy()
+
+        # Search for a similar piece of code if RAG is enabled
+        if use_rag and self.vectorstore:
+            docs = self.vectorstore.similarity_search(prompt)
+
+            prompt = "# Relevant context " + "\n".join(docs[i].page_content for i in range(len(docs))) + "\n" + prompt
+
         payload["messages"].append({"role": "user", "content": prompt})
 
         url = self.config["url"]
@@ -66,11 +137,11 @@ class AutoComplete:
 
         return code
 
-    def exec_prompt(self, start_line, end_line, prompt, replace=False) -> None:
+    def exec_prompt(self, start_line, end_line, prompt, replace=False, use_rag=False) -> None:
         buffer_lines = vim.current.buffer
-        prompt = self.get_selection(buffer_lines, start_line, end_line) + "\n" + prompt
+        prompt = prompt + "\n" + self.get_selection(buffer_lines, start_line, end_line)
 
-        out = self.query_model(prompt)
+        out = self.query_model(prompt, use_rag)
 
         code = self.parse_code(out)
 
@@ -78,7 +149,8 @@ class AutoComplete:
         if replace:
             del buffer_lines[start_line - 1 : end_line]
         else: 
-            start_line = end_line + 1
+            start_line = end_line
+
         vim.api.buf_set_lines(
             buffer_lines,
             start_line - 1,
@@ -88,23 +160,25 @@ class AutoComplete:
         )
 
     def comment(self, start_line, end_line) -> None:
-        prompt = "Rewrite the code above, line by line, by adding documentation and comments."
+        prompt = "\nTask: Rewrite the code below, line by line, by adding documentation and comments."
         vim.async_call(
             self.exec_prompt,
             start_line=start_line,
             end_line=end_line,
             prompt=prompt,
             replace=True,
+            use_rag=False,
         )
 
     def autocomplete(self, start_line, end_line) -> None:
-        prompt = "Continue the implementation of this piece of code."
+        prompt = "\nTask: Continue the implementation of this piece of code: "
         vim.async_call(
             self.exec_prompt,
             start_line=start_line,
             end_line=end_line,
             prompt=prompt,
             replace=False,
+            use_rag=True,
         )
 
     def postprocess(self, out):
